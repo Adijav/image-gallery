4c4c33c95a5ea5e5ec21f160478347553f6d0adb
us-guest <us-guest@us-guests-MacBook-Pro.local>
2020-02-15 00:21:54 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import PeopleCard from './peopleCard';
 
 function App() {
+  const [users, setUsers] = useState(0);
+  
+  useEffect(()=>{
+    async function fetchData() {
+      setUsers(
+        await fetch('https://reqres.in/api/users?page=2')
+        .then(res => res.json())
+        .then(res => res)
+      )
+    }
+    fetchData();
+
+    // const promise = new Promise((resolve, reject) => {
+    //   fetch('https://reqres.in/api/users?page=2')
+    //   .then(res => res.json())
+    //   .then(res => resolve(res))
+    //   .catch(rej => reject(rej))
+    // })
+
+    // promise.then(res => setUsers(res));
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <h3>Material UI</h3>
+      <Grid
+        container
+        spacing={10}
+        styles={{padding: '24px'}}
+      > 
+      {users && users.data.map(user => (
+        <Grid
+          key={user.id}
+          item
+          xs={12} sm={6} md={4} lg={4} xl={3}
         >
-          Learn React
-        </a>
-      </header>
+          <PeopleCard
+            email={user.email}
+            firstName={user.first_name}
+            lastName={user.last_name}
+            avatar={user.avatar}
+          />
+        </Grid>
+      ))}
+
+      </Grid>
     </div>
   );
 }
